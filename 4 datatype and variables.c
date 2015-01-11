db022401f6438cf7d8f00788ed89237d176b4fa5
Alim Ul Karim <alim@developers-organism.com>
2015-01-12 00:16:20 +0600

    updated-2

---
+++ 4 datatype and variables.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    char cCharacterExample = 'd';
+    char cCharacterexample;
+
+    cCharacterexample = 'x';
+    int x = 550 , y= 250;
+    int X = 500 , Y= 200;
+    printf("%c %c\n" , cCharacterExample, cCharacterexample);
+    printf("%d %d\n" , x, y);
+    printf("%d %d\n" , X+x, Y+y);
+    printf("%d %d\n" , X*x, Y*y);
+    printf("%d %d\n" , printf("%d %d\n" , X*x, Y*y);X/x, Y/y);
+    getch();
+    return 0;
+}
