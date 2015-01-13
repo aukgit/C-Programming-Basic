808305dcc32e84a2e7d427f01e11bff9f4e786cc
Alim Ul Karim <alim@developers-organism.com>
2015-01-14 02:16:02 +0600

    updated-4

---
+++ 5 relational operators.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){    int x = 39;
+    bool condition = x == 0;
+
+    printf("%d" , condition);
+
+    getch();
+    return 0;
+}
---
+++ 5 relational operators.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){    int x = 39;
+    bool condition = false || false && true && false || (true && false || false && (true && false || false));
+
+    printf("%d" , condition);
+
+    getch();
+    return 0;
+}
