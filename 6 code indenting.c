2789819e5f8d789eae40b0d9185a11624c893610
Alim Ul Karim <auk.junk@live.com>
2015-01-15 17:46:36 +0600

    indenting

---
+++ 6 code indenting.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+// Alim Ul Karim
+// indenting
+// C, C++, C#, Java,
+// JavaScript, Visual Basic,
+// Perl, Python, ActionScript, ...
+int main() {//right js
+
+
+    printf("Hello World");
+    getch();
+    return 0;
+}
+
