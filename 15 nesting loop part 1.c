69531980f12605bd5a0f5fe6e3ed5246a0cc4889
Alim Ul Karim <auk.junk@live.com>
2015-01-25 12:05:58 +0600

    array reverse and swapping

--- 15 nesting loop part 1.c
+++ 15 nesting loop part 1.c
@@ -12,16 +12,7 @@ void reverse_array(int num[], int size){
 }
 
 int main() {
-    //int x = 5 , y = 9;
-    int numbers = {1,4,5,6,7,10};
 
-
-    int temp;
-    temp = x;
-    x = y;
-    y = temp;
-
-    printf("x:%d y:%d" , x, y);
 
     getchar();
     return 0;
