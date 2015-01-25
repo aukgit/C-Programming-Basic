88dd6a65b3553d6f0eea36a4882b290519be860d
Alim Ul Karim <auk.junk@live.com>
2015-01-25 12:00:50 +0600

    array reversing and swapping

---
+++ 15 nesting loop part 2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+// Alim Ul Karim
+// swapping
+// nesting loops behaviour
+// reversing
+// sorting
+
+void swapping(int num[], int index1, int index2){
+}
+void reverse_array(int num[], int size){
+}
+
+int main() {
+    //int x = 5 , y = 9;
+    int numbers = {1,4,5,6,7,10};
+
+
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
+
+    printf("x:%d y:%d" , x, y);
+
+    getchar();
+    return 0;
+}
+
+
+
